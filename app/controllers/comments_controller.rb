25681fcf66797b32706605718a2904fb855b919f
Hong Phuc <hongphuc.dthp@gmail.com>
2022-03-17 04:24:40 +0700

    add Comment mailer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,20 @@ class CommentsController < ApplicationController
   before_action :set_post
 
   def create
-    @post.comments.create!(
-      params.require(:comment).permit(:content)
-    )
+    comment = @post.comments.create!(comment_params)
+
+    CommentsMailer.submitted(comment).deliver_later
 
     redirect_to @post
   end
 
   private
 
-    def set_post
-      @post = Post.find(params[:post_id])
-    end
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
